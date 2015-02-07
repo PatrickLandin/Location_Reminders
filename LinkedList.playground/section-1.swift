b96cf2c89c6d20700ebf0c51e13956705ba80f94
Patrick Landin <patricklandin@gmail.com>
2015-02-06 16:50:38 -0800

    Added linked list playground

--- LinkedList.playground/section-1.swift
+++ LinkedList.playground/section-1.swift
@@ -2,5 +2,49 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class Node {
+  
+  var next : Node?
+  var data : Int?
+  
+  func addNode(node : Node) {
+    if self.next == nil {
+      self.next = node
+    } else {
+      self.next!.addNode(node)
+    }
+  }
+}
 
+class LinkedList {
+  
+  var head : Node?
+  
+  func insertValue(value : Int) {
+    var node = Node()
+    node.data = value
+    
+    if self.head == nil {
+      self.head = node
+    } else {
+      insertAtHead(node)
+    }
+  }
+  
+  func insertAtHead(node : Node) {
+    node.next = self.head
+    self.head = node
+  }
+  
+  func insertAtEnd(node : Node) {
+    head!.addNode(node)
+  }
+  
+  func insertAtEndWithWhile(node : Node) {
+    var nextNode = self.head
+    while nextNode!.next != nil {
+      nextNode = nextNode!.next
+    }
+    nextNode!.next = node
+  }
+}
